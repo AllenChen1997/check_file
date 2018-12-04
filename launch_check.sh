11708227fe44741f0287f73c616e2ce68f6c2a49
AllenChen1997 <a86792511@gmail.com>
2018-12-04 21:30:05 +0800

    let it auto get the setting mass values

--- launch_check.sh
+++ launch_check.sh
@@ -5,6 +5,7 @@
 # need to set magicCard.txt
 #############################
 #load some function
+
 function compare_fn { # this is used for float point value compare
 	local i=0
 	# $1 = filename
@@ -14,16 +15,16 @@ function compare_fn { # this is used for float point value compare
 		((i+=1))
 		#echo "i=$i"
 		local getV=$(grep "$(echo $m|sed -e s/"#"/"# "/g)"$ $aFile |tr -s ' ' |cut -d ' ' -f 3)
-		#echo "getV=$getV"
+		echo "getV=$getV"
 		local c=$(echo $fn_2|cut -d ' ' -f $i)
-		#echo "c=$c"
+		echo "c=$c"
 		local res=$(awk 'BEGIN{if ('$getV' == '$c') print 1; else print 0}')
-		#echo "res=$res"
+		echo "res=$res"
 		if [ $res != 1 ]; then
 			flag=false
 		fi
 	done
-}	
+}
 
 function PDF_weight_fn {
 	> tmpfile.txt
@@ -51,8 +52,19 @@ function PDF_weight_fn {
 	done
 	c=$(grep "no find PDF=" tmpfile.txt |cut -d '=' -f 2)
 	if [ ! -z $(echo $c|tr -d ' ') ]; then
-		echo "no find PDF= $c" >> ${newName}_scan.txt
+		echo "no find PDF= $c" >> ${saveName}.txt
 	fi
+}
+
+function get_mCompare_value {
+	local i=0
+	for i in $mCompaP; do
+		mCompa=$(echo $aFile|sed -e "s:$place::g" |cut -d '_' -f $i |tr -d 'a-zA-Z')
+		echo "mC=$mCompa"
+		fn_1=$mList
+		fn_2=$mCompa
+		compare_fn   
+	done		
 }
 
 #define variables
@@ -62,16 +74,16 @@ echo "Start process"
 
 # define variables
 place=$(grep "#place" magicCard |cut -d ':' -f 2) # read the setting in the magic card
-keyWord=$(grep "#key word" magicCard |cut -d ':' -f 2|tr -d " ")  # read the setting in the magic card
+saveName=$(grep "#save file name" magicCard |cut -d ':' -f 2|tr -d " ")  # read the setting in the magic card
 SetID=$(grep "#PID" magicCard |cut -d ':' -f 2)
 mList=$(grep "#mass list" magicCard |cut -d ':' -f 2 |tr -d " "|tr "|" " ") # mass list for mass compare sys.
-mCompa=$(grep "#mass compare" magicCard |cut -d ':' -f 2 |tr -d " "|tr "|" " ") # mass compare for mass compare sys.
+mCompaP=$(grep "#mass compare" magicCard |cut -d ':' -f 2 |tr -d " "|tr "|" " ") # mass compare for mass compare sys.
 mwList=$(grep "#mass width list" magicCard |cut -d ':' -f 2 |tr -d " "|tr "|" " ") # mass width list for mass compare sys.
 mwCompa=$(grep "#mass width compare" magicCard |cut -d ':' -f 2 |tr -d " "|tr "|" " ") # mass width compare for mass compare sys.
 lhaid=$(grep "lhaid" magicCard |cut -d ':' -f 2| tr -d ' ')
 process=$(grep "process" magicCard |cut -d ':' -f 2| tr -d ' ')
 PDFWeight=$(grep "#PDF weight" magicCard |cut -d ':' -f 2|tr -d ' '|tr '|' " ")
-filename=$(find $place -type f -name "$keyWord*.lhe")  # this shows more of "how to use find() "
+filename=$(find $place/j*/c*.lhe ) 
 dictory=`pwd` # record where is this dictory
 
 
@@ -83,12 +95,8 @@ if [ -z "$filename" ]; then  # test if filename is a zero length variables
 fi
 
 #build scan file
-# if scan file name(newName) becomes strange, please modify this line
-newName=$keyWord$(echo $filename|cut -d "_" -f 1 |cut -d $keyWord -f 2)  
-> ${newName}_scan.txt  # if the file already exist, clean it 
-# newName=$keyWord   #if the problem can not be solve, you can use keyword as scan file name(use this line)
-###################################################################
-echo newName=$newName
+> ${saveName}.txt  # if the file already exist, clean it 
+echo "save name=$saveName"
 
 # do the check in lhe file
 declare -a getV #used store the data get from lhe file
@@ -99,9 +107,7 @@ for aFile in $filename; do
 	# setting mass check
 	echo "start $aFile mass check"
 	if [ ! -z $mList ]; then #check weather mList is empty or not
-		fn_1=$mList
-		fn_2=$mCompa
-		compare_fn   
+		get_mCompare_value
 	else echo "pass the setting mass check"
 	fi	
 	#((length+=$i))
@@ -125,11 +131,19 @@ for aFile in $filename; do
 
 	# defualt check (lhaid / process)
 	echo "start $aFile defualt check"
-	getLHAID=$(grep "lhaid" $aFile |cut -d '=' -f 1)
-	getProc=$(grep ^generate $aFile |sed -e s/generate//g |tr -d ' ')
-	getV=($getLHAID $getProc)
-	if [ $getLHAID != $lhaid ] || [ $getProc != $process ]; then 
+	if [ ! -z $lhaid ]; then
+		getLHAID=$(grep "lhaid" $aFile |cut -d '=' -f 1)
+		if [ $getLHAID != $lhaid ]; then
 			flag=false
+		fi
+	else echo "pass lhaid check"	
+	fi	
+	if [ ! -z $process ]; then
+		getProc=$(grep ^generate $aFile |sed -e s/generate//g |tr -d ' ')
+		if [ $getProc != $process ]; then 
+			flag=false
+		fi
+	else echo "pass process check"	
 	fi
 	
 	# see the final state of flag and print result into tmpfile
@@ -140,21 +154,19 @@ for aFile in $filename; do
 		echo "$aFile false" >> tmpfile.txt
 	fi
 # output
-#echo $list >> ${newName}_scan.txt
-column -t tmpfile.txt >> ${newName}_scan.txt
+column -t tmpfile.txt >> ${saveName}.txt
 done
-
 
 
 # check if want to run root analysis
 willRoo=$(grep "#root analysis" magicCard|cut -d ':' -f 2|tr -d ' ')
 if [ $willRoo = "N" ]; then
-	echo "${newName}_scan.txt has build"
+	echo "${saveName}.txt has build"
 	echo "process done"
 	exit 1
 fi
 	echo "start root analysis"
-	echo "the result in root analysis" >> ${newName}_scan.txt
+	echo "the result in root analysis" >> ${saveName}.txt
 	> tmpfile.txt
 	# change the mother PID in .C file
 	placeInC=$(grep -n "define motherPID" ./TRootLHEFParticle.C|cut -d ":" -f 1) #find the line we want to change
@@ -174,10 +186,10 @@ fi
 	# output
 		ans1=$(grep "mass test" ./tmpfile.txt)
 		ans2=$(grep "fail" ./tmpfile.txt)
-		echo " " >> ${newName}_scan.txt
-		echo -n " $b;" >> ${newName}_scan.txt
-		echo -n " $ans1;" >> ${newName}_scan.txt
-		echo -n " $ans2;" >> ${newName}_scan.txt
+		echo " " >> ${saveName}.txt
+		echo -n " $b;" >> ${saveName}.txt
+		echo -n " $ans1;" >> ${saveName}.txt
+		echo -n " $ans2;" >> ${saveName}.txt
 	done
-	echo "${newName}_scan.txt has build"
+	echo "${saveName}_scan.txt has build"
 	echo "process done"
